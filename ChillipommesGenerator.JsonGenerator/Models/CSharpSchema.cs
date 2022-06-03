9686133716341f6d88b16cb4e86447b106ddc5d3
Hendrik Sandberg <hennesaba123@gmail.com>
2022-06-03 13:45:32 +0200

    CSharp Schema finished

--- ChillipommesGenerator.JsonGenerator/Models/CSharpSchema.cs
+++ ChillipommesGenerator.JsonGenerator/Models/CSharpSchema.cs
@@ -2,17 +2,32 @@
 
 namespace ChillipommesGenerator.JsonGenerator.Models
 {
+    /// <summary>
+    /// Class to define the Schema for C-Sharp specific data properties and layouts
+    /// </summary>
     internal class CSharpSchema
     {
+        /// <summary>
+        /// Determines if the class is sealed
+        /// </summary>
         [JsonPropertyName("sealed")]
         public bool Sealed { get; set; }
 
+        /// <summary>
+        /// Detemrines if the class is static
+        /// </summary>
         [JsonPropertyName("static")]
         public bool Static { get; set; }
 
+        /// <summary>
+        /// The base class from which it should inherit 
+        /// </summary>
         [JsonPropertyName("inheritFromClass")]
         public string InheritFromClass { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The interfaces from which it should inherit
+        /// </summary>
         [JsonPropertyName("inheritFromInterfaces")]
         public string[] InheritFromInterfaces { get; set; } = Array.Empty<string>();
     }
